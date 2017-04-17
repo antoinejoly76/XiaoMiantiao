c81e1ed55d7cf6284aaee8c7c395156a7f864714
antoinejoly <jolyant@gmail.com>
2017-04-17 21:55:25 +0100

    Fixed flickr algo and active buttons

--- public/angular/xm.js
+++ public/angular/xm.js
@@ -77,6 +77,29 @@ var FlickrbuildPicCtrl = function(FlickrsearchChinaPictures, ValidateLandscapePi
 
 };
 
+var StyleCtrl = function($scope) {
+    $scope.class1 = "";
+    $scope.class2 = "active";
+    $scope.class3 = "";
+    $scope.changeClass = function(btn) {
+        console.log("CALLER " + btn + "BEFORE - class 1 " + $scope.class1 + " class2" + $scope.class2 + " class3" + $scope.class3)
+        if (btn === 'btn1') {
+            $scope.class1 = "active";
+            $scope.class2 = "";
+            $scope.class3 = "";
+        } else if (btn === 'btn2') {
+            $scope.class2 = "active";
+            $scope.class1 = "";
+            $scope.class3 = "";
+        } else if (btn === 'btn3') {
+            $scope.class3 = "active";
+            $scope.class1 = "";
+            $scope.class2 = "";
+        }
+        console.log("AFTER - class 1 " + $scope.class1 + " class2" + $scope.class2 + " class3" + $scope.class3)
+    }
+}
+
 
 var resultsData = function($http) {
     this.runSearch = function(param) {
@@ -106,34 +129,46 @@ var FlickrbuildPicCtrlv2 = function(FlickrsearchChinaPictures, ValidateLandscape
     };
 
 
-    console.log($scope.tabpics);
+    // console.log($scope.tabpics);
     FlickrsearchChinaPictures.runSearch().async().then(function(d) {
 
         for (i = 0; i < 10; i++) {
+            if (checkPicOK(d.data[i])) {
+                $scope.tabpics['pics'].push({
+                    'url': 'https://farm' + d.data[i].farm + '.staticflickr.com/' + d.data[i].server + '/' + d.data[i].id + '_' + d.data[i].secret + '_n.jpg',
+                    'title': d.data[i].title,
+                    'id': d.data[i].id,
+                    'isLandscape': ""
+                });
+                console.log("Pic " + i + "'s url is OK")
+            } else {
+                console.log("Pic " + i + "'s url is NOT OK")
+                  $scope.tabpics['pics'].push(
+                    {
+                      'url': null,
+                      'title': null,
+                      'id': null,
+                      'isLandscape': null,
+                  });
+            }
 
-            $scope.tabpics['pics'].push({
-                'url': 'https://farm' + d.data[i].farm + '.staticflickr.com/' + d.data[i].server + '/' + d.data[i].id + '_' + d.data[i].secret + '_n.jpg',
-                'title': d.data[i].title,
-                'id': d.data[i].id,
-                'isLandscape': ""
+        }
+
+        var number = 0;
+
+        for (i = 0; i < 10; i++) {
+            ValidateLandscapePictures.isLandscape($scope.tabpics['pics'][i].id).async().then(function(d) {
+                // console.log($scope.tabpics['pics']);
+                $scope.tabpics['pics'][number].isLandscape = d.isLandscape;
+                number++;
+
+                if (number == 10) {
+                    finalise();
+                }
             });
         }
 
-    var number = 0;
-
-    for (i = 0; i < 10; i++) {
-        ValidateLandscapePictures.isLandscape($scope.tabpics['pics'][i].id).async().then(function(d) {
-           console.log(  $scope.tabpics['pics']);
-            $scope.tabpics['pics'][number].isLandscape = d.isLandscape;
-            number++;
-
-            if (number == 10) {
-                finalise();
-            }
-        });
-    }
-
-      });
+    });
 
 
 
@@ -159,11 +194,17 @@ var FlickrbuildPicCtrlv2 = function(FlickrsearchChinaPictures, ValidateLandscape
             }
 
         }
-
+        if (i === pics.length) i--; // We went through 10 pictures and did not find any landscape. We send the last picture by default
         $scope.getChinaPicUrl = pics[i].url;
         $scope.getChinaPicTitle = pics[i].title;
 
     }
+
+    function checkPicOK(pic) {
+        if (pic) return true;
+        else return false;
+    }
+
 
 }
 
@@ -181,16 +222,17 @@ var FlickrsearchChinaPictures = function($http) {
                     console.log(response.data.photos.photo[randpic]);
                     return {
                         'data': [
-                                response.data.photos.photo[randpic-4],
-                                response.data.photos.photo[randpic - 3],
-                                response.data.photos.photo[randpic - 2],
-                                response.data.photos.photo[randpic - 1],
-                                response.data.photos.photo[randpic],
-                                response.data.photos.photo[randpic + 1],
-                                response.data.photos.photo[randpic + 2],
-                                response.data.photos.photo[randpic + 3],
-                                response.data.photos.photo[randpic + 4],
-                                response.data.photos.photo[randpic + 5]]
+                            response.data.photos.photo[randpic - 4],
+                            response.data.photos.photo[randpic - 3],
+                            response.data.photos.photo[randpic - 2],
+                            response.data.photos.photo[randpic - 1],
+                            response.data.photos.photo[randpic],
+                            response.data.photos.photo[randpic + 1],
+                            response.data.photos.photo[randpic + 2],
+                            response.data.photos.photo[randpic + 3],
+                            response.data.photos.photo[randpic + 4],
+                            response.data.photos.photo[randpic + 5]
+                        ]
                     };
 
 
@@ -209,20 +251,28 @@ var ValidateLandscapePictures = function($http) {
         var myService = {
             async: function() {
                 console.log(picId);
+                if (!picId) return {
+                    isLandscape: 'false'
+                };
+
                 var promise = $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=ae08eba93a977d220945ff2d08f960bb&photo_id=' + picId + '&format=json&nojsoncallback=1').then(function(response) {
                     console.log(response.data);
                     console.log(response.data.sizes.size[4].width)
                     console.log(response.data.sizes.size[4].height)
                     var width = response.data.sizes.size[4].width;
                     var height = response.data.sizes.size[4].height;
-                    if (width = 320 && height <= 215) //We want a ratio of 1.5 or wider
+
+                    if (width == 320 && height <= 240) { //We want a ratio of 1.5 or wider
+                        console.log("is LAndscape")
                         return {
                             isLandscape: 'true'
                         }
-                    else
+                    } else {
+                        console.log("is NOT  LAndscape")
                         return {
                             isLandscape: 'false'
                         }
+                    }
                 });
                 return promise;
             }
@@ -237,4 +287,4 @@ var ValidateLandscapePictures = function($http) {
 
 
 myApp.controller('resultsListCtrl', resultsListCtrl).controller('inputSearchCtrl', inputSearchCtrl).controller('queryInfoCtrl', queryInfoCtrl).service('resultsData', resultsData).controller('FlickrbuildPicCtrl', FlickrbuildPicCtrl).service('ValidateLandscapePictures', ValidateLandscapePictures).service('FlickrsearchChinaPictures', FlickrsearchChinaPictures)
-    .controller('FlickrbuildPicCtrlv2', FlickrbuildPicCtrlv2)
+    .controller('FlickrbuildPicCtrlv2', FlickrbuildPicCtrlv2).controller('StyleCtrl', StyleCtrl);
